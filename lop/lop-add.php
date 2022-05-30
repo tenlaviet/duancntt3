c62c02112e1a7633dc5e9bad210d49f5feda2a0f
KhanhVu12 <ankain1210@gmail.com>
2022-05-30 17:22:20 +0700

    add class styled

--- lop/lop-add.php
+++ lop/lop-add.php
@@ -57,39 +57,46 @@ disconnect_db();
         <title>Thêm lớp</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet" />
+        <link rel="icon" type="image/png" href="../img/Logo-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="../img/Logo-16x16.png" sizes="16x16" />
+        <link href="../styles/sidebar.css" rel="stylesheet" type="text/css" />
+        <link href="../styles/header.css" rel="stylesheet" type="text/css" />
+        <link href="../styles/table.css" rel="stylesheet" type="text/css" />
+        <script src="https://kit.fontawesome.com/19fbdee3eb.js" crossorigin="anonymous"></script>
     </head>
     <body>
-        <h1>Thêm lớp</h1>
-        <a href="lop-list.php">Trở về</a> <br/> <br/>
-        <form method="post" action="lop-add.php">
-            <table width="50%" border="1" cellspacing="0" cellpadding="10">
-                <tr>
-                    <td>Mã lớp</td>
-                    <td>
-                        <input type="text" name="malop" value=""/>
-                        <?php if (!empty($errors['MaLop'])) echo $errors['MaLop'];?>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Mã chuyên ngành</td>
-                    <td>
-                        <input type="text" name="macn" value=""/>
-                        <?php if (!empty($errors['MaCn'])) echo $errors['MaCn'];?>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>Khóa đào tạo</td>
-                    <td>
-                        <input type="text" name="name" value=""/>
-                        <?php if (!empty($errors['KhoaDaoTao'])) echo $errors['KhoaDaoTao']; ?>
-                <tr>
-                    <td></td>
-                    <td>
-                        <input type="submit" name="add_lop" value="Lưu"/>
-                    </td>
-                </tr>
-            </table>
-        </form>
+        <?php include 'C:\xampp\htdocs\duancntt3\component\admin-sidebar.php';?>
+        <div class="wrapper">
+            <h1>Thêm lớp</h1>
+            <form method="post" action="lop-add.php" class="table-wrapper">
+                <table class="verticle-table">
+                    <tr>
+                        <th>Mã lớp</th>
+                        <td>
+                            <input type="text" name="malop" value=""/>
+                            <?php if (!empty($errors['MaLop'])) echo $errors['MaLop'];?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Mã chuyên ngành</th>
+                        <td>
+                            <input type="text" name="macn" value=""/>
+                            <?php if (!empty($errors['MaCn'])) echo $errors['MaCn'];?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Khóa đào tạo</th>
+                        <td>
+                            <input type="text" name="name" value=""/>
+                            <?php if (!empty($errors['KhoaDaoTao'])) echo $errors['KhoaDaoTao']; ?>
+                    <tr>
+                </table>
+                <input type="submit" name="add_lop" value="Lưu" class="save button"/>
+            </form>
+        </div>
+        <script src="../scripts/dropdown.js"></script>
     </body>
 </html>
